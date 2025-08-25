88405d01723cb4030a1b1f74e76cb0f2a84d767f
tahmeedhasan88 <hasantahmeed416@gmail.com>
2025-08-25 20:52:14 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Login button
+document.getElementById("login").addEventListener("click", function(e){
+    e.preventDefault()
+    const mobileNumber= 01733055918
+    const pinNumber= 223344
+    const mobileValue= parseInt(document.getElementById("mblno").value)
+
+    const pinValue= parseInt(document.getElementById("pin").value)
+
+
+    if(mobileNumber===mobileValue && pinNumber===pinValue){
+       window.location.href="secondpg.html"
+    }
+    else{
+        alert("Invalid Recognization")
+    }   
+
+})
