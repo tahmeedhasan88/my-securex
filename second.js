15fb79d647654569c213c7a7372dc4353a7c8bc7
tahmeedhasan88 <hasantahmeed416@gmail.com>
2025-08-26 16:03:35 +0600

    allmost completed

--- second.js
+++ second.js
@@ -18,3 +18,63 @@ document.getElementById("trnsfr").addEventListener("click", function(){
 
     document.getElementById("out").style.display="none"
 })
+
+
+// Additional calculation
+
+
+document.getElementById("add-btn").addEventListener("click",
+    function(e){
+        e.preventDefault()
+
+        const inputAmount= parseInt(document.getElementById("moneyadd").value) 
+        const mainBalance= parseInt(document.getElementById("available-amount").innerText)
+
+        const totalBalance= inputAmount+mainBalance
+
+        document.getElementById("available-amount").innerText=totalBalance
+        
+        alert("You added TK")
+
+    }
+)
+
+
+document.getElementById("cashout-btn").addEventListener("click",
+    function(e){
+        e.preventDefault()
+
+        const inputAmount= parseInt(document.getElementById("cashout-amount").value) 
+        const mainBalance= parseInt(document.getElementById("available-amount").innerText)
+
+        const totalBalance=mainBalance-inputAmount
+
+        document.getElementById("available-amount").innerText=totalBalance
+        
+        alert("Cashout completed successfully")
+
+    }
+)
+
+
+document.getElementById("trnsfr-btn").addEventListener("click",
+    function(e){
+        e.preventDefault()
+
+        const inputAmount= parseInt(document.getElementById("transfer-amount").value) 
+        const mainBalance= parseInt(document.getElementById("available-amount").innerText)
+
+        const totalBalance=mainBalance-inputAmount
+
+        document.getElementById("available-amount").innerText=totalBalance
+        
+        alert("Transfered successfully")
+
+    }
+)
+
+
+
+document.getElementById("logout-btn").addEventListener("click", function(){
+    window.location.href="index.html"
+})
